566cb2e47e9640249fd9adcb4506650374998fe1
HUHU0101123 <youcancallmecaro@gmail.com>
2024-08-10 23:40:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,29 +107,36 @@ else:
                                    title='Ventas vs Ganancias Después de Impuestos',
                                    template='plotly_dark')
         fig_sales_trends.for_each_trace(lambda t: t.update(name='Ventas' if t.name == 'Total' else 'Ganancias Después de Impuestos'))
-        fig_sales_trends.update_layout(showlegend=False)
-        fig_sales_trends.update_layout(
-            annotations=[
-                dict(
-                    x=sales_trends['Month'].iloc[-1],  # Posición X para la anotación
-                    y=sales_trends['Total'].iloc[-1],  # Posición Y para la anotación
-                    text='Ventas',
-                    showarrow=True,
-                    arrowhead=2,
-                    ax=0,
-                    ay=-40
-                ),
-                dict(
-                    x=sales_trends['Month'].iloc[-1],  # Posición X para la anotación
-                    y=sales_trends['Ganancia Después de Impuestos'].iloc[-1],  # Posición Y para la anotación
-                    text='Ganancias Después de Impuestos',
-                    showarrow=True,
-                    arrowhead=2,
-                    ax=0,
-                    ay=-40
-                )
-            ]
-        )
+
+    # Calcular la posición media para cada curva
+    midpoints = {
+        'Ventas': sales_trends.loc[sales_trends['Total'] == sales_trends['Total'].max() // 2, 'Month'].values[0],
+        'Ganancias Después de Impuestos': sales_trends.loc[sales_trends['Ganancia Después de Impuestos'] == sales_trends['Ganancia Después de Impuestos'].max() // 2, 'Month'].values[0]
+    }
+
+    fig_sales_trends.update_layout(showlegend=False)
+    fig_sales_trends.update_layout(
+        annotations=[
+            dict(
+                x=midpoints['Ventas'],  # Posición X para la anotación
+                y=sales_trends[sales_trends['Month'] == midpoints['Ventas']]['Total'].mean(),  # Posición Y para la anotación
+                text='Ventas',
+                showarrow=True,
+                arrowhead=2,
+                ax=0,
+                ay=-40
+            ),
+            dict(
+                x=midpoints['Ganancias Después de Impuestos'],  # Posición X para la anotación
+                y=sales_trends[sales_trends['Month'] == midpoints['Ganancias Después de Impuestos']]['Ganancia Después de Impuestos'].mean(),  # Posición Y para la anotación
+                text='Ganancias Después de Impuestos',
+                showarrow=True,
+                arrowhead=2,
+                ax=0,
+                ay=-40
+            )
+        ]
+    )
         st.plotly_chart(fig_sales_trends)
     else:
         st.write("No hay datos suficientes para mostrar tendencias de ventas.")
