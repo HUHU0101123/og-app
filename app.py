78c152eb7db747a1268f8c20f1d4953199ebad75
HUHU0101123 <youcancallmecaro@gmail.com>
2024-08-19 16:48:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,18 @@ import streamlit as st
 from pagina_importaciones import pagina_importaciones
 from pagina_ventas import pagina_ventas
 
+# Configuración de la página (debe estar al inicio del archivo)
+st.set_page_config(page_title="Dashboard de Ventas", layout="wide")
+
 # Título de la aplicación
-st.set_page_config(page_title="Mi Dashboard")
-
-# Función para mostrar la página de importaciones
-def show_importaciones():
-    pagina_importaciones()
-
-# Función para mostrar la página de ventas
-def show_ventas():
-    pagina_ventas()
+st.title("Mi Dashboard")
 
 # Menú de navegación lateral
 page = st.sidebar.selectbox("Selecciona una página", ["Importaciones", "Ventas"])
 
 # Mostrar la página seleccionada
 if page == "Importaciones":
-    show_importaciones()
+    pagina_importaciones()
 elif page == "Ventas":
-    show_ventas()
-
+    pagina_ventas()
 
